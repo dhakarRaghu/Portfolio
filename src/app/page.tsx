eb0a9b059f4b71145d1b83bd94305749b21a6177
dhakarRaghu <rdhakad2002@gmail.com>
2025-07-24 08:45:23 +0530

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import Hero from "../components/Hero"
 import About from "../components/About"
-import Achievements from "../components/Achievements"
+import Achievements from "../components/Achievements"  
 import Projects from "../components/Projects"
-import Contact from "../components/Contact"
-import Experience from "../components/Experience"
-import Footer from "../components/Footer"
+import Contact from "../components/contact"
+import Experience from "../components/experience"
+import Footer from "../components/footer"
 import Navigation from "../components/Navigation"
 import FloatingElements from "../components/ui/FloatingElements"
 
